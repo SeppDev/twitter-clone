b53f9c5a86a7bc07f9e5528392bce5ea29ef7cba
Radwanfa <86819098+Radwanfa@users.noreply.github.com>
2024-10-02 16:04:48 +0200

    update UI

--- login.php
+++ login.php
@@ -1,5 +1,25 @@
+<?php
+session_start();
+include "php/database.php";
+global $db;
+?>
 <html>
+<head>
+    <title>Chirpify</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
 <body>
-    <p>Hello, world!</p>
+<div class="wrapper">
+<div class="tweet">
+    <div class="profile">
+        <div class="pfp"><img src="https://i.pinimg.com/474x/39/a2/5c/39a25cdfd53c68f2f9d904e623896c45.jpg" width="60px" height="60px"></div>
+        <div class="name">jhwigdnjn</div>
+    </div>
+    <div class="content">
+        uguyewgdidetgfuetjffgf4
+    </div>
+</div>
+</div>
 </body>
 </html>
