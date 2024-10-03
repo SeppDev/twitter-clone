a741dbcb52cd8d898a9a5a787cb8a35524134939
SeppDev <70803492+SeppDev@users.noreply.github.com>
2024-10-03 10:31:47 +0200

    ja

--- src/login.php
+++ src/login.php
@@ -1,32 +1,48 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles/basic.css">
     <title>Document</title>
 </head>
 
 <body>
-    <div>
 
+<?php
+    echo random_bytes(32);
+?>
+    
+    <div id="login-container" style="display: flex; flex-direction: column">
         <p>username</p>
         <input type="user" id="username">
         <p>passsword</p>
         <input type="password" id="password">
-        <button type="submit" id="submit">Submit</button>
+        <button type="submit" id="SignUp">Sign Up</button>
+        <button id="Login">Login</button>
+
 
         <script>
-            const button = document.getElementById("submit");
+            const sign_up_button = document.getElementById("SignUp");
+            const login_button = document.getElementById("Login");
+
             const username = document.getElementById("username");
             const password = document.getElementById("password");
 
-            
+            login_button.onclick = async () => {
+                const response = await fetch("api/login", {
+                    method: "POST",
+                    headers: {
+                        Username: username.value,
+                        Password: password.value
+                    }
+                });
+                console.log(response);
+            }
 
-            button.onclick = () => {
-                fetch("api/accounts.php", {
+            sign_up_button.onclick = async () => {
+                const response = await fetch("api/create_account", {
                     method: "POST",
                     headers: {
                         Username: username.value,
