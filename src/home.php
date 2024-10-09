ea6ab1440d92faf53562e9b30130f7611a87348c
Radwanfa <86819098+Radwanfa@users.noreply.github.com>
2024-10-09 11:07:00 +0200

    trash

--- src/home.php
+++ src/home.php
@@ -8,8 +8,6 @@ require "modules/database.php";
 <head>
     <title>Chirpify</title>
     <link rel="stylesheet" href="styles/style.css">
-    <script src="js/main.js" defer>
-    </script>
 </head>
 
 <body>
@@ -45,7 +43,48 @@ require "modules/database.php";
         }
         ?>
     </div>
+    <script>
+        const logout = document.getElementById("logout");
+        let dialog = document.getElementById("dialog");
+        let Submit = document.getElementById("submit");
+        let content = document.getElementById("content");
+        let wrapper = document.getElementById("wrapper");
+        console.log(wrapper);
+        logout.onclick = async () => {
+            const response = await fetch("api/logout", {
+                method: "POST",
+            })
+            document.cookie = "session_token=";
+            window.location.reload();
+        }
 
+        function tweet() {
+            dialog.open = true;
+        }
+        function submit(event) {
+            event.preventDefault();
+        }
+        container.onsubmit = submit
+        Submit.onclick = async () => {
+            const response = await fetch("api/post", {
+                method: "POST",
+                headers: {
+                    content: content.value
+                }
+            });
+            dialog.open = false;
+            await handle_response(response);
+        }
+        async function handle_response(response) {
+            let json = await response.json();
+            const posts = json.posts;
+            wrapper.innerHTML = "";
+            console.log(posts);
+            for (let i = 0; i<posts.length; i++) {
+                wrapper.innerHTML += posts[i];
+            }
+        }
+    </script>
 </div>
 </body>
 
